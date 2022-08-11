482c657460e2f1575e2a2dc2e0441854950240c1
Karla <pangilinan.karla@gmail.com>
2022-08-11 15:54:52 +0800

    Integrate Socket.io into iOS project

--- SwiftUISocketIO/SocketIOApp/Shared/ContentView.swift
+++ SwiftUISocketIO/SocketIOApp/Shared/ContentView.swift
@@ -6,10 +6,41 @@
 //
 
 import SwiftUI
+import SocketIO
 
 struct ContentView: View {
+  @StateObject private var service = SocketService()
+
   var body: some View {
-    Text("Hello, world!")
+    VStack(spacing: 20) {
+      Text("Received messages from Node.js:")
+        .font(.title)
+        .padding(.horizontal)
+      
+      Button("Connect") {
+        service.establishConnection()
+      }
+      
+      Button("Disconnect") {
+        service.closeConnection()
+      }
+      
+      Button("Move") {
+        service.sendMove()
+      }
+
+      if !service.message.isEmpty {
+        Text("Message received: \"\(service.message)\"")
+      }
+      
+      Text("Moves sent: \(service.moves.count)")
+      Form {
+        ForEach(service.moves) { move in
+          Text("\(move.id ?? "")")
+        }
+      }
+      Spacer()
+    }
   }
 }
 
---
+++ SwiftUISocketIO/SocketIOApp/Shared/SocketService.swift
@@ -0,0 +1,62 @@
+//
+//  SocketService.swift
+//  SocketIOApp
+//
+//  Created by Karla Pangilinan on 8/11/22.
+//
+
+import Foundation
+import SocketIO
+
+final class SocketService: ObservableObject {
+  // TODO: ❗️ Replace ip to test ❗️
+  private var manager = SocketManager(
+    //    socketURL: URL(string: "ws://localhost:3000")!,
+    socketURL: URL(string: "http://192.168.x.xxx:3000")!,
+    config: [.log(true), .compress]
+  )
+  
+  @Published var moves: [Move] = .init()
+  @Published var message: String = ""
+  
+  init() {
+    let socket = manager.defaultSocket
+    socket.on(clientEvent: .connect) { data, ack in
+      print("Connected")
+      socket.emit("NodeJS Server Port", "Hi, NodeJS server!")
+    }
+    
+    socket.on("iOS Client Port") { [weak self] data, ack in
+      if let data = data.first as? [String: String],
+         let message = data["msg"] {
+        DispatchQueue.main.async {
+          self?.message = message
+        }
+      }
+    }
+    
+    socket.on("Move received") { [weak self] data, ack in
+      print("Move received \(data)")
+      if let data = data.first as? Data,
+         let move: Move = data.decodeToObject() {
+        DispatchQueue.main.async {
+          self?.moves.append(move)
+        }
+      }
+    }
+  }
+  
+  func establishConnection() {
+    manager.defaultSocket.connect()
+  }
+  
+  func closeConnection() {
+    manager.defaultSocket.disconnect()
+  }
+  
+  func sendMove() {
+    let move = Move(moveUp: true, moveDown: false)
+    guard let jsonData = try? JSONEncoder().encode(move) else { return }
+    manager.defaultSocket.emit("Send move", jsonData)
+  }
+}
