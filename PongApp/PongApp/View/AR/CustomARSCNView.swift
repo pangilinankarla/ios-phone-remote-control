c68d2db2e04a27791595a5b827c6f755decf8785
Karla <mariakarla.pangilinan@quipper.com>
2022-12-01 14:17:53 +0800

    Update CustomARSCNView

--- PongApp/PongApp/View/AR/CustomARSCNView.swift
+++ PongApp/PongApp/View/AR/CustomARSCNView.swift
@@ -11,23 +11,23 @@ import SceneKit
 
 final class CustomARSCNView: ARSCNView {
   private let validExpressions: [Expression] = [
-    NeutralExpression(),
-    LookLeftExpression(),
-    LookRightExpression(),
-    LookUpExpression(),
-    LookDownExpression(),
-    BlinkExpression(),
-    BlinkLeftExpression(),
-    BlinkRightExpression(),
+    Neutral(),
+    LookLeft(),
+    LookRight(),
+    LookUp(),
+    LookDown(),
+    Blink(),
+    BlinkLeft(),
+    BlinkRight(),
     Angry(),
     Joy(),
     Fun(),
     Sorrow(),
-    LetterAExpression(),
-    LetterIExpression(),
-    LetterUExpression(),
-    LetterEExpression(),
-    LetterOExpression(),
+    LetterA(),
+//    LetterE(),
+//    LetterI(),
+//    LetterO(),
+//    LetterU(),
   ]
   var logger: ARSCNViewLogger?
   var service: SocketService?
@@ -36,9 +36,9 @@ final class CustomARSCNView: ARSCNView {
   private var currentBlendShape: BlendShape?
 }
 
-extension CustomARSCNView: ARSessionDelegate, ARSCNViewDelegate {
+// MARK: - Public methods
+extension CustomARSCNView {
   func setupForFaceTracking() {
-    //    guard ARFaceTrackingConfiguration.isSupported else { return }
     let configuration = ARFaceTrackingConfiguration()
     if #available(iOS 13.0, *) {
       configuration.maximumNumberOfTrackedFaces = ARFaceTrackingConfiguration.supportedNumberOfTrackedFaces
@@ -48,7 +48,10 @@ extension CustomARSCNView: ARSessionDelegate, ARSCNViewDelegate {
     
     self.delegate = self
   }
-  
+}
+
+// MARK: - ARSCNViewDelegate
+extension CustomARSCNView: ARSCNViewDelegate {
   func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
     guard
       let sceneView = renderer as? ARSCNView,
@@ -57,63 +60,118 @@ extension CustomARSCNView: ARSessionDelegate, ARSCNViewDelegate {
 
     let faceGeometry = ARSCNFaceGeometry(device: sceneView.device!)
     let material = faceGeometry!.firstMaterial!
-    
+
     material.diffuse.contents = #imageLiteral(resourceName: "wireframeTexture")
     material.lightingModel = .physicallyBased
-    
-    //    let node = SCNNode(geometry: faceMash)
-    //    node.geometry?.firstMaterial?.fillMode = .lines
+
     let node = SCNNode(geometry: faceGeometry)
-    
+
     return node
   }
-  
+
   func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
     guard
       let faceAnchor = anchor as? ARFaceAnchor,
       let faceGeometry = node.geometry as? ARSCNFaceGeometry
     else { return }
-    
+
     faceGeometry.update(from: faceAnchor.geometry)
-    let result = readMyFace(anchor: faceAnchor)
 
-    if expressionString != result.0 {
-      expressionString = result.0
-      print(expressionString)
-      logger?.log(expressionString)
+    let newBlendShape = readMyFace(anchor: faceAnchor)
+
+    if newBlendShape != currentBlendShape {
+      currentBlendShape = newBlendShape
+      sendMovement(newBlendShape)
     }
 
-    if !(expressions == result.1) {
-      expressions.removeAll()
-      result.1.forEach { expressions.append($0.copy() as! Expression) }
-      print("New expressions: \(expressions)\n")
-      
-      let newBlendShape = BlendShape.generateFromExpressions(expressions)
-      
-      if newBlendShape != currentBlendShape {
-        currentBlendShape = newBlendShape
-//        service?.sendBlendShapes(newBlendShape)
-        service?.sendMovement(
-          Movement(
-            roomId: "12345abc",
-            sessionId: "456xyz",
-            character: Character(
-              blendShape: newBlendShape
-            )
-          )
-        )
-      }
-    }
+  }
+}
+
+import Accelerate
+
+// MARK: - Private methods
+extension CustomARSCNView {
+  private func readMyFace(anchor: ARFaceAnchor) -> BlendShape {
+    let blendShape = BlendShape(
+      a: validExpressions.first(where: { $0 is LetterA })?.getValue(from: anchor) ?? 0.0,
+      angry: validExpressions.first(where: { $0 is Angry })?.getValue(from: anchor) ?? 0.0,
+      blink: validExpressions.first(where: { $0 is Blink })?.getValue(from: anchor) ?? 0.0,
+      blinkLeft: validExpressions.first(where: { $0 is BlinkLeft })?.getValue(from: anchor) ?? 0.0,
+      blinkRight: validExpressions.first(where: { $0 is BlinkRight })?.getValue(from: anchor) ?? 0.0,
+      e: validExpressions.first(where: { $0 is LetterE })?.getValue(from: anchor) ?? 0.0,
+      fun: validExpressions.first(where: { $0 is Fun })?.getValue(from: anchor) ?? 0.0,
+      i: validExpressions.first(where: { $0 is LetterI })?.getValue(from: anchor) ?? 0.0,
+      joy: validExpressions.first(where: { $0 is Joy })?.getValue(from: anchor) ?? 0.0,
+      lookDown: validExpressions.first(where: { $0 is LookDown })?.getValue(from: anchor) ?? 0.0,
+      lookLeft: validExpressions.first(where: { $0 is LookLeft })?.getValue(from: anchor) ?? 0.0,
+      lookRight: validExpressions.first(where: { $0 is LookRight })?.getValue(from: anchor) ?? 0.0,
+      lookUp: validExpressions.first(where: { $0 is LookUp })?.getValue(from: anchor) ?? 0.0,
+      neutral: validExpressions.first(where: { $0 is Neutral })?.getValue(from: anchor) ?? 0.0,
+      o: validExpressions.first(where: { $0 is LetterO })?.getValue(from: anchor) ?? 0.0,
+      sorrow: validExpressions.first(where: { $0 is Sorrow })?.getValue(from: anchor) ?? 0.0,
+      u: validExpressions.first(where: { $0 is LetterU })?.getValue(from: anchor) ?? 0.0,
+      unknown: 0
+    )
+
+    return blendShape
   }
 
-  func readMyFace(anchor: ARFaceAnchor) -> (String, [Expression]) {
-    let defaultString = "(No expression)"
-    var faceResultArray: [String] = []
-    
-    let result = !faceResultArray.isEmpty ? "You are \(faceResultArray.joined(separator: ", "))" : defaultString
+  private func sendMovement(_ blendShape: BlendShape?) {
+    guard let blendShape else { return }
 
-    let expressionArr: [Expression] = validExpressions.filter { $0.isExpressing(from: anchor) && !$0.isDoingWrongExpression(from: anchor) }
+    service?.sendMovement(
+      Movement(
+        roomId: "12345abc",
+        sessionId: "456xyz",
+        character: Character(
+          blendShape: blendShape
+        )
+      )
+    )
+  }
 
-    return (result, expressionArr)
+  private func testSendMovement() {
+    let bShape = BlendShape(
+      a: 0,
+      angry: 0,
+      blink: 0,
+      blinkLeft: 0,
+      blinkRight: 0,
+      e: 0,
+      fun: 0,
+      i: 0,
+      joy: 0,
+      lookDown: 0,
+      lookLeft: 0,
+      lookRight: 0,
+      lookUp: 0,
+      neutral: 0,
+      o: 0,
+      sorrow: 0,
+      u: 0,
+      unknown: 0
+    )
+
+    print("*** TEST BLEND SHAPE: \(bShape)\n")
+
+    sendMovement(bShape)
+  }
+}
+
+extension ARFaceAnchor {
+  func blendShapeDoubleValue(_ blendShape: ARFaceAnchor.BlendShapeLocation, max: Double? = nil) -> Double {
+    let doubleValue = blendShapes[blendShape]?.doubleValue.roundToPlaces(4) ?? 0.0
+
+    if let max, doubleValue > max {
+      return max
+    } else {
+      return doubleValue
+    }
+  }
+}
+
+extension Double {
+  func withThreshold(_ threshold: Double) -> Double {
+    return (self > threshold) ? threshold : self
   }
 }
